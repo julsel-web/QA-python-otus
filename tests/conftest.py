8e4924fe469a50e4f9990ede7f3e4e50012dd6fe
Julia Tuz <jul.sel@findmykids.org>
2026-06-03 09:08:21 +0400

    работа с бд

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,5 @@
 import pytest
+import pymysql
 import requests
 
 
@@ -28,6 +29,11 @@ def json_api():
 def pytest_addoption(parser):
     parser.addoption("--url",  default="https://ya.ru", help="This is request url")
     parser.addoption("--code", type=int, default=200, help="This is request code")
+    parser.addoption("--db-host", default="localhost", help="MariaDB host")
+    parser.addoption("--db-port", type=int, default=3306, help="MariaDB port")
+    parser.addoption("--db-name", default="opencart", help="Database name")
+    parser.addoption("--db-user", default="opencart", help="Database user")
+    parser.addoption("--db-password", default="opencart", help="Database password")
 
 
 @pytest.fixture(scope="session")
@@ -40,12 +46,26 @@ def base_code(request):
     code = request.config.getoption("--code")
     return code
 
+
+@pytest.fixture(scope="session")
+def connection(request):
+    connection = pymysql.connect(
+        host=request.config.getoption("--db-host"),
+        port=request.config.getoption("--db-port"),
+        user=request.config.getoption("--db-user"),
+        password=request.config.getoption("--db-password"),
+        database=request.config.getoption("--db-name"),
+        cursorclass=pymysql.cursors.DictCursor,
+        autocommit=False,
+    )
+    yield connection
+    connection.close()
+
 def test_status_code(base_url, base_code):
     url = base_url
     code = base_code
     response = requests.get(url)
     assert response.status_code == code
-
 
 
 
